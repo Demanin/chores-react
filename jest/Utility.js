faada404da0af4c453f9306bf8ca53ef6ef5a222
Demanin <cigeerd@gmail.com>
2019-08-10 18:19:19 -0500

    Added getRandomBool to jest utility

--- jest/Utility.js
+++ jest/Utility.js
@@ -3,16 +3,21 @@ import crypto from 'crypto';
 
 const getRandomString = (length = 20) => {
   return crypto.randomBytes(length).toString('hex');
-}
+};
 
 const getRandomNumber = (min = 0, max = 9999999) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
+
+const getRandomBool = () => {
+  return !!getRandomNumber(0, 1);
+};
 
 export default {
   getRandomString,
   getRandomNumber,
+  getRandomBool,
 };
